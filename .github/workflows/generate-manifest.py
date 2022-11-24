3045b77e62df6ca9c69d48963cadd41869eff496
Meno Abels <meno.abels@adviser.com>
2022-11-24 12:10:13 +0100

    work around the interferance

--- .github/workflows/generate-manifest.py
+++ .github/workflows/generate-manifest.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 import argparse
 import yaml
+import pathlib 
+import subprocess
 
-def generate_manifest(image, tag, src_tag):
+def generate_manifest(src_image, dst_image):
     return {
-            "image": f"{image}:{tag}",
+            "image": f"{dst_image}",
             "manifests": [
                 {
-                    "image": f"{image}:{src_tag}_aarch64",
+                    "image": f"{src_image}_aarch64",
                     "platform": {
                         "architecture": "arm64",
                         "os": "linux",
@@ -15,7 +17,7 @@ def generate_manifest(image, tag, src_tag):
                     }
                 },
                 {
-                    "image": f"{image}:{src_tag}_x86_64",
+                    "image": f"{src_image}_x86_64",
                     "platform": {
                         "architecture": "amd64",
                         "os": "linux"
@@ -24,11 +26,30 @@ def generate_manifest(image, tag, src_tag):
             ]
     }
 
+
 parser = argparse.ArgumentParser(prog = 'generate-manifest', description = 'generate a manifest')
+
+parser.add_argument('--build_yaml')
 
 parser.add_argument('--image')
 parser.add_argument('--tag')
 parser.add_argument('--src_tag')
 
 args = parser.parse_args()
-print(yaml.dump(generate_manifest(image=args.image, tag=args.tag, src_tag=args.src_tag)))
+
+#print("hello")
+print(args.build_yaml)
+fcontent = pathlib.Path(args.build_yaml).read_text()
+#print(fcontent)
+build = yaml.full_load(fcontent)
+#print(build)
+for name, val in build['services'].items():
+    res = subprocess.run(['bash', '-c', f"echo -n {val['image']}"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+    #print(res)
+    dst_image = f"{':'.join(res.split(':')[:-1])}:lastest"
+    print(yaml.dump(generate_manifest(dst_image=dst_image, src_image=res)))
+    #bash -c "echo \"${DOCKER_ORG:-mailu}/${DOCKER_PREFIX:-}nginx:${PINNED_MAILU_ARCH_VERSION:-local}\""
+#mailu/nginx:local
+#
+
+
